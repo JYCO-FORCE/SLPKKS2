406b141460b88b7b23a1ea0b5282128d8578f82f
JYCO-FORCE <58536365+JYCO-FORCE@users.noreply.github.com>
2025-01-16 10:54:47 +0100

    SLPKKS.js

--- SLPKKS.js
+++ SLPKKS.js
@@ -12,8 +12,9 @@ var version = "V1.1 2025-01-15 12:44";
 
 var CLP ;
 
-//var cas1 = 15; var cas2 = 15; var cas3 = 15; var cas4 = 1;
-setTimeout(function(){
+var cas1 = 15; var cas2 = 15; var cas3 = 15; var cas4 = 1;
+
+/*setTimeout(function(){
 
 const fiAppointmentType2 = localStorage.getItem("fiAppointmentType"+localStorage.getItem("TLS_WEB_issuer"));
 var typo2 = "";
@@ -21,26 +22,26 @@ var typo2 = "";
 if (fiAppointmentType2 && typeof fiAppointmentType2 === "string") {
 
 if (fiAppointmentType2.includes("PRIMO") || fiAppointmentType2.includes("primo") || fiAppointmentType2.includes("Primo") || fiAppointmentType2.includes("privee") ) { // CLP = cas1;
-  typo2 = "PRIMO" + window.location.pathname.split("/")[3]; 
+  typo2 = "PRIMO" + window.location.pathname.split("/")[3];
   if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 7; } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 15;  } else {  CLP = 10;  }
 } else if (fiAppointmentType2.includes("VISE") || fiAppointmentType2.includes("vise") || fiAppointmentType2.includes("Vise") || fiAppointmentType2.includes("visé")) { // CLP = cas2;
   typo2 = "VISE" + window.location.pathname.split("/")[3];
-  if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 15;  } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 15;  } else {  CLP = 15;  }  
+  if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 15;  } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 15;  } else {  CLP = 15;  }
 } else if (fiAppointmentType2.includes("Renouvellement") || fiAppointmentType2.includes("renouvellement") || fiAppointmentType2.includes("RENOUVELLEMENT")) {  // CLP = cas2;
   typo2 = "VISE" + window.location.pathname.split("/")[3];
-  if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 15;  } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 15;  } else {  CLP = 15;  }    
+  if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 15;  } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 15;  } else {  CLP = 15;  }
 } else if (fiAppointmentType2.includes("CIRCULATION") || fiAppointmentType2.includes("circulation") || fiAppointmentType2.includes("Circulation")) { // CLP = cas3;
-  typo2 = "CIRCULATION" + window.location.pathname.split("/")[3]; 
-  if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 20;  } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 20;  } else {  CLP = 20;  }                                                                                                                                                      
+  typo2 = "CIRCULATION" + window.location.pathname.split("/")[3];
+  if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 20;  } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 20;  } else {  CLP = 20;  }
 } else if (fiAppointmentType2.includes("Long Sejour") || fiAppointmentType2.includes("long sejour")) {
-  typo2 = "Longsejour"; 
-} else {   if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 15;  } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 15;  } else {  CLP = 15;  }  
+  typo2 = "Longsejour";
+} else {   if (window.location.pathname.split("/")[3] === "maCAS2fr" ) {  CLP = 15;  } else if (window.location.pathname.split("/")[3] === "maOUD2fr" ) { CLP = 15;  } else {  CLP = 15;  }
   typo2 = fiAppointmentType2.replace(/\s/g, "") + window.location.pathname.split("/")[3];
   console.log("null");
 }
     console.log(CLP);
                     }
- }, 2000);
+ }, 2000);*/
 
 if((document.body.innerText).indexOf('This XML file does not appear to have any style information associated with it. The document tree is shown below.') > -1){ setTimeout(function(){ location.reload(); document.body.style.backgroundColor = "#FFA500"; }, 60e3); };
 if((document.body.innerText).indexOf('You have tried to log in too many times. For security reasons, you are blocked. Please re-connect later.') > -1){       setTimeout(function(){ window.location = "https://ps.w.org/limit-login-attempts-reloaded/assets/banner-1544x500.png?rev=2954981"; }, 60e3);   }
@@ -221,7 +222,7 @@ setTimeout(function () { window.location.href = window.location.href;  }, 10e3);
 if (response.status === 500) {
 
 setTimeout(function () { window.location.href = window.location.href;  }, 80e3);
-    
+
 //let url = window.location.href;
 //let newUrl = url.replace("appointment", "personal");
 //console.log(newUrl);
@@ -378,7 +379,7 @@ const disconnect403 = 3;
         //setTimeout(function(){ ERROR403(); },50e3);
         tt = Math.floor(Math.random() * 10);
 
-    } else if (parseInt(localStorage.getItem("inputi403")) > disconnect403) {  
+    } else if (parseInt(localStorage.getItem("inputi403")) > disconnect403) {
                 localStorage.setItem("inputi403", 0);
         document.getElementById("MOTIF").textContent = "votre table statut : " + response.status + " blocked ";
         console.error("statut : " + response.status);
@@ -406,7 +407,7 @@ setTimeout(function () { window.location.href = window.location.href; }, 1e3);
             else
 
    if (response.status === 500) {
-       
+
        setTimeout(function () { window.location.href = window.location.href;  }, 30e3);
 
 //let url = window.location.href;
@@ -630,7 +631,7 @@ let previousIndex = -1; // initialize previousIndex to an invalid value
 let randomIndex = Math.floor(Math.random() * removedButtons.length);
 while (randomIndex === previousIndex) {  randomIndex = Math.floor(Math.random() * removedButtons.length);}
 previousIndex = randomIndex;
-    const randomButton = removedButtons[randomIndex];      
+    const randomButton = removedButtons[randomIndex];
  setTimeout(function(){   randomButton.click(); },1e3);
 }  else { setTimeout(function(){ window.location.href = window.location },5e3); }
 }, 1000); },1e3); },1e3); }
@@ -2073,7 +2074,7 @@ let randomIndex = Math.floor(Math.random() * removedButtons.length);
 while (randomIndex === previousIndex) {  randomIndex = Math.floor(Math.random() * removedButtons.length);}
 previousIndex = randomIndex;    const randomButton = removedButtons[randomIndex];  const randomButton2 = removedButtons[Math.floor(Math.random() * removedButtons.length)];
 setTimeout(function(){  randomButton2.click(); },1e3);
-}  
+}
             else {
   consecutiveErrors++;
  setTimeout(function(){ location.reload();  },80e3);
